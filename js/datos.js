774e4c0b95da0b327b7364fd7a9afe2eaecdadff
Barbara Correa <marcorrea2@gmail.com>
2023-10-11 23:28:44 -0300

    Cambie el codigo

---
+++ js/datos.js
@@ -0,0 +1,58 @@
+const productos = [
+    { 
+        id: 1,
+        tipo: 'Amigurumi',
+        nombre: 'Coneja',
+        precio: 3800,
+        imagen: 'utilities/img/amigurumi/conejita.jpeg'
+    },
+    {
+        id: 2,
+        tipo: 'Amigurumi',
+        nombre: 'Elefante',
+        precio: 3800,
+        imagen: 'utilities/img/amigurumi/elefante.jpeg'
+    },
+    {
+        id: 3,
+        tipo: 'Amigurumi',
+        nombre: 'Osito',
+        precio: 3800,
+        imagen: 'utilities/img/amigurumi/osito.jpeg'
+    },
+    {
+        id: 4,
+        tipo: 'Amigurumi',
+        nombre: 'Mapache',
+        precio: 3800,
+        imagen: 'utilities/img/amigurumi/mapache.jpg'
+    },
+    {
+        id: 5,
+        tipo: 'Amigurumi',
+        nombre: 'Monito',
+        precio: 3800,
+        imagen: 'utilities/img/amigurumi/monito.jpg'
+    },
+    {
+        id: 6,
+        tipo: 'manta',
+        nombre: 'Manta Coneja',
+        precio: 5200,
+        imagen: 'utilities/img/mantas/manta-coneja.jpg'
+    },
+    {
+        id: 7,
+        tipo: 'manta',
+        nombre: 'Manta Elefante',
+        precio: 5200,
+        imagen: 'utilities/img/mantas/manta-elefante.jpg'
+    },
+    {
+        id: 8,
+        tipo: 'sonajero',
+        nombre: 'Sonajero Conejo',
+        precio: 2100,
+        imagen: 'utilities/img/sonajeros/sonajero-conejo.jpg'
+    }
+];
--- js/main.js
+++ js/main.js
@@ -1,48 +1,142 @@
-const carritoDeCompras = [];
+let carritoDeCompras = [];
+
+let itemProductos = document.getElementById("productos");
+let carrito = document.getElementById("carrito");
+let total = document.getElementById("total");
+let vaciar = document.getElementById("vaciar");
+
+
+//Funciones
+
+function renderizarProductos(){
+    productos.forEach((producto)=>{
+        //Cuerpo del card nuevo Producto
+        const nuevoDiv = document.createElement('div');
+        nuevoDiv.classList.add('card','col-sm-4');
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        //Titulo
+        const titulo = document.createElement('h4');
+        titulo.classList.add('card-title');
+        titulo.textContent = producto.nombre;
+        //Tipo
+        const tipo = document.createElement('h5');
+        tipo.textContent = producto.tipo;
+        //Imagen
+        const imagen = document.createElement('img');
+        imagen.classList.add('img-fluid');
+        imagen.setAttribute('src', producto.imagen);
+        //Precio
+        const precio = document.createElement('p');
+        precio.classList.add('card-text');
+        precio.textContent = `$ ${producto.precio}`;
+
+        //Boton agregar al carrito el producto
+        const agregar = document.createElement('button');
+        agregar.classList.add('btn', 'btn-primary');
+        agregar.textContent = 'Agregar al Carrito';
+        agregar.setAttribute('marcador', producto.id);
+        agregar.addEventListener('click', agregarAlCarrito);
+
+        //Agregamos cada valor del objeto producto a su card correspondiente
+        cardBody.appendChild(titulo);
+        cardBody.appendChild(tipo);
+        cardBody.appendChild(imagen);
+        cardBody.appendChild(precio);
+        cardBody.appendChild(agregar);
+        nuevoDiv.appendChild(cardBody);
+
+        //Agregamos al nodo padre 
+        itemProductos.appendChild(nuevoDiv);
+
+
+    });
+}
+
 
 
 //Evento del Carrito de compras
-let comprar = document.getElementsByClassName("comprar");
+function agregarAlCarrito (e){
+    carritoDeCompras.push(e.target.getAttribute('marcador'));
+    renderizarCarrito();
+};
 
 
+
+//Dibuja en la pantalla los productos guardados en el carrito
+
+function renderizarCarrito(){
+    carrito.textContent = '';
+    const productoUnico = [...new Set (carritoDeCompras)];
+    //Genera los nodos a partir del carrito
+    productoUnico.forEach((item) => {
+        //obtiene el item que necesita de los datos de  productos
+        const itemCarrito = productos.filter((itemProductos) => {
+            return itemProductos.id === parseInt(item);
+        }); 
+    
+    //Nodo del item del producto
+    const nuevoItem = document.createElement('li');
+    nuevoItem.classList.add('text-right','mx-2');
+    nuevoItem.textContent = `Producto Seleccionado: ${itemCarrito[0].nombre} - Precio: ${itemCarrito[0].precio}`;
+
+    //Boton eliminar un item del carrito
+    const botonEliminar = document.createElement('button');
+    botonEliminar.classList.add('btn','btn-danger');
+    botonEliminar.textContent ='Eliminar';
+    botonEliminar.addEventListener('click', eliminarItemCarrito);
+
+    nuevoItem.appendChild(botonEliminar);
+    carrito.appendChild(nuevoItem);
+    });
+
+    total.textContent = calcularTotal();
+}
+
+
+//Evento para Eliminar un item del carrito
+function eliminarItemCarrito(e) {
+    // Obtener el producto ID que hay en el boton pulsado
+
+
+    //se vuelve a renderizar
+    renderizarCarrito();
+}
+
+
+
+//Calcular el precio Total del carrito
+
+function calcularTotal() {
+    let total = 0;
+ //continuar
+    return total += producto.precio;
+}
+
+
+
+//Vaciar todo el carrito completo 
+function vaciarCarrito(){
+    carrito = [];
+    renderizarCarrito();
+};
+
+
+//Evento
+vaciar.addEventListener('click', vaciarCarrito);
+
+
+//Llamado de funciones
+ renderizarProductos();
+ renderizarCarrito();
+
+
+/*
 let mensaje = document.getElementById("mensaje");
 const mostrarMensaje = ()=>{
     if(carritoDeCompras.length === 0){
         mensaje.className = "visible";
     }else{mensaje.className ="oculto";}
 }
-
-
-comprar.addEventListener('click', agregarAlCarrito =>{
-    mostrarMensaje();
-    carritoDeCompras.push({}); //aca se deberia agregar el producto correspondiente al array carritoDeCompras
-});
-
-
-//Guardado del carrito de Compras en el Storage
-const carritoStorage = (clave,valor) => {localStorage.setItem(clave,valor)};
-    for(const carrito of carritoDeCompras) {
-        carritoStorage("carrito",JSON.stringify(carritoDeCompras));
-    }
-
-
-
-//Mostrar productos seleccionados
-
-/*Obtener carrito de productos seleccionados */
-let carritoFinal = JSON.stringify(localStorage.getItem('carrito'));
-
-/* referencia a la seccion donde se mostrarán los productos:*/
-let productosSeleccionados = document.getElementById("productosSeleccionados")
-
-const mostrarProductosCarrito = ()=>{
-    for(const carrito of carritoFinal){
-        let productoCarrito = document.createElement("div");
-        productoCarrito.innerHTML = `<p>
-          Tipo: ${carrito.tipo} 
-          Producto: ${carrito.nombre}  
-          Precio: ${carrito.precio}</p>`;
-        document.productosSeleccionados.append(productoCarrito);
-    }
-}
-
+*/
